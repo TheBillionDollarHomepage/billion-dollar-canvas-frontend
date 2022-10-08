9f047a05defb5efafe1175362f6679b95a9bf4a9
outdoteth <outdoteth@users.noreply.github.com>
2022-10-08 18:05:07 -0500

    update env

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { formatEther, parseEther } from "ethers/lib/utils";
 import Head from "next/head";
 import { createContext, useContext, useEffect, useState } from "react";
 import styled from "styled-components";
-import { useAccount, useBlockNumber, useSigner } from "wagmi";
+import { useAccount, useBlockNumber, useProvider, useSigner } from "wagmi";
 import { Input } from "../components/core/Input";
 import { Modal } from "../components/layout/Modal";
 import billionDollarCanvasAbi from "../contracts/BillionDollarCanvas.abi.json";
@@ -23,6 +23,8 @@ const Container = styled.div`
       height: 200px;
     }
   }
+
+  margin-bottom: 48px;
 `;
 
 const PixelContainer = styled.div`
@@ -52,6 +54,7 @@ const Pixel = ({ id }) => {
   const [tokenUri, setTokenUri] = useState();
   const [price, setPrice] = useState();
   const { data: signer } = useSigner();
+  const provider = useProvider();
   const [pixels, loading, error] = useContext(PixelsContext);
   const [pixelPrice, setPixelPrice] = useState();
 
@@ -60,12 +63,12 @@ const Pixel = ({ id }) => {
   const canvas = new Contract(
     process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
     billionDollarCanvasAbi,
-    signer
+    signer || provider
   );
 
   const buyPixel = async () => {
     // submit transaction to buy pixel
-    const tx = await canvas.buy(id, "", parseEther("0.01"), {
+    const tx = await canvas.buy(id, pixel.tokenUri || "", pixelPrice, {
       value: pixelPrice,
     });
     await tx.wait();
@@ -112,9 +115,9 @@ const Pixel = ({ id }) => {
             {pixel.tokenUri && <img src={pixel.tokenUri} />}
 
             {pixel.owner && <p>Owner: {pixel.owner}</p>}
-            <p>Price: {formatEther(pixelPrice || "0")} ETH</p>
+            <p>Harberger tax price: {formatEther(pixelPrice || "0")} ETH</p>
 
-            {address?.toLowerCase() === pixel.owner && (
+            {address && address.toLowerCase() === pixel.owner && (
               <>
                 <div className="input-and-label">
                   <label htmlFor="data-uri">Data URI</label>
@@ -129,7 +132,7 @@ const Pixel = ({ id }) => {
                 <button onClick={() => updateDataUri()}>Update data URI</button>
 
                 <div className="input-and-label">
-                  <label htmlFor="data-uri">Price (ETH)</label>
+                  <label htmlFor="data-uri">Harberger tax price (ETH)</label>
                   <Input
                     id="data-uri"
                     placeholder="Enter ETH amount..."
